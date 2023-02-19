5f4d032d0a9e3177eb853dcdc777c225c604c1c6
David Håland <david.f.haland@gmail.com>
2023-02-19 22:00:17 +0100

    documentation added

--- after/plugin/coc.lua
+++ after/plugin/coc.lua
@@ -49,3 +49,17 @@ keyset("n", "gd", "<Plug>(coc-definition)", { silent = true, desc = "go to defin
 keyset("n", "gy", "<Plug>(coc-type-definition)", { silent = true, desc = "type definition" })
 keyset("n", "gi", "<Plug>(coc-implementation)", { silent = true, desc = "go to implementation" })
 keyset("n", "gr", "<Plug>(coc-references)", { silent = true, desc = "show references" })
+--
+-- Use K to show documentation in preview window
+function _G.show_docs()
+    local cw = vim.fn.expand('<cword>')
+    if vim.fn.index({ 'vim', 'help' }, vim.bo.filetype) >= 0 then
+        vim.api.nvim_command('h ' .. cw)
+    elseif vim.api.nvim_eval('coc#rpc#ready()') then
+        vim.fn.CocActionAsync('doHover')
+    else
+        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+    end
+end
+
+keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', { silent = true })
