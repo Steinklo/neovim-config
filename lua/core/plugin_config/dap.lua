e9eebd4b612c15958d8e2218e6491f52b63a8ab9
David Håland <david.f.haland@gmail.com>
2023-06-25 20:12:34 +0200

    debug remap

--- lua/core/plugin_config/dap.lua
+++ lua/core/plugin_config/dap.lua
@@ -1,23 +1,19 @@
 local dap = require('dap')
 
-dap.adapters.coreclr = {
-    type = 'executable',
-    command = 'C:/Users/david/AppData/Local/bin/netcoredbg/netcoredbg.exe',
-    args = { '--interpreter=vscode' }
-}
-
-dap.configurations.cs = {
-    {
-        type = "coreclr",
-        name = "launch - netcoredbg",
-        request = "launch",
-        program = function()
-            return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
-        end,
+-- Go
+require('dap-go').setup {
+    dap_configurations = {
+        {
+            type = "go",
+            name = "Attach remote",
+            mode = "remote",
+            request = "attach",
+        },
     },
 }
-
-vim.keymap.set('n', '<Leader>c' function() require('dap').continue() end, { desc = "Debug continue" })
+-- Keymap
+vim.keymap.set('n', '<leader>du', '<cmd>lua require("dapui").toggle()<cr>', { desc = "Toggle DapUI" })
+vim.keymap.set('n', '<Leader>c', function() require('dap').continue() end, { desc = "Debug continue" })
 vim.keymap.set('n', '<Leader>j', function() require('dap').step_over() end, { desc = "Debug step over" })
 vim.keymap.set('n', '<Leader>k', function() require('dap').step_back() end, { desc = "Debug step back" })
 vim.keymap.set('n', '<Leader>l', function() require('dap').step_into() end, { desc = "Debug step into" })
@@ -25,91 +21,8 @@ vim.keymap.set('n', '<Leader>h', function() require('dap').step_out() end, { des
 vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end, { desc = "Debug toggle breakpoint" })
 
 -- UI
-
+require("dapui").setup()
 local dap, dapui = require("dap"), require("dapui")
-
-dapui.setup({
-    icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
-    mappings = {
-        -- Use a table to apply multiple mappings
-        expand = { "<CR>", "<2-LeftMouse>" },
-        open = "o",
-        remove = "d",
-        edit = "e",
-        repl = "r",
-        toggle = "t",
-    },
-    -- Use this to override mappings for specific elements
-    element_mappings = {
-        -- Example:
-        -- stacks = {
-        --   open = "<CR>",
-        --   expand = "o",
-        -- }
-    },
-    -- Expand lines larger than the window
-    -- Requires >= 0.7
-    expand_lines = vim.fn.has("nvim-0.7") == 1,
-    -- Layouts define sections of the screen to place windows.
-    -- The position can be "left", "right", "top" or "bottom".
-    -- The size specifies the height/width depending on position. It can be an Int
-    -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
-    -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
-    -- Elements are the elements shown in the layout (in order).
-    -- Layouts are opened in order so that earlier layouts take priority in window sizing.
-    layouts = {
-        {
-            elements = {
-                -- Elements can be strings or table with id and size keys.
-                { id = "scopes", size = 0.25 },
-                "breakpoints",
-                "stacks",
-                "watches",
-            },
-            size = 40, -- 40 columns
-            position = "left",
-        },
-        {
-            elements = {
-                "repl",
-                "console",
-            },
-            size = 0.25, -- 25% of total lines
-            position = "bottom",
-        },
-    },
-    controls = {
-        -- Requires Neovim nightly (or 0.8 when released)
-        enabled = true,
-        -- Display controls in this element
-        element = "repl",
-        icons = {
-            pause = "",
-            play = "",
-            step_into = "",
-            step_over = "",
-            step_out = "",
-            step_back = "",
-            run_last = "↻",
-            terminate = "□",
-        },
-    },
-    floating = {
-        max_height = nil, -- These can be integers or a float between 0 and 1.
-        max_width = nil, -- Floats will be treated as percentage of your screen.
-        border = "single", -- Border style. Can be "single", "double" or "rounded"
-        mappings = {
-            close = { "q", "<Esc>" },
-        },
-    },
-    windows = { indent = 1 },
-    render = {
-        max_type_length = nil, -- Can be integer or nil.
-        max_value_lines = 100, -- Can be integer or nil.
-    }
-})
-
-
 dap.listeners.after.event_initialized["dapui_config"] = function()
     dapui.open()
 end
