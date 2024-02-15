406e4b979243a3ba7a6a707c5a3af0073499e492
Steinklo <65152981+Steinklo@users.noreply.github.com>
2024-02-15 13:48:35 +0100

    clipboardremap added

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,9 +6,13 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Remap to clipboard
-vim.keymap.set({ "n", "v" }, "<leader>p", [["*p]], { desc = "Paste from clipboard" })
-vim.keymap.set({ "n", "v" }, "<leader>y", [["*y]], { desc = "Yank to clipboard" })
-vim.keymap.set({ "n", "v" }, "<leader>d", [["*d]], { desc = "Cut to clipboard" })
+vim.keymap.set({ "n", "v" }, "p", [["*p]])
+vim.keymap.set({ "n", "v" }, "P", [["*P]])
+vim.keymap.set({ "n", "v" }, "y", [["*y]])
+vim.keymap.set({ "n", "v" }, "Y", [["*Y]])
+vim.keymap.set({ "n", "v" }, "d", [["*d]])
+vim.keymap.set({ "n", "v" }, "D", [["*D]])
+vim.keymap.set({ "n", "v" }, "x", [["*x]])
 
 -- Text editing
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace text" })
