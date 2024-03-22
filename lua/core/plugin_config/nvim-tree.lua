a51100706db1de019c8ce47d97984cd97ef3028f
Steinklo <65152981+Steinklo@users.noreply.github.com>
2024-03-22 08:43:04 +0100

    nvim tree closes on open file

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -4,10 +4,14 @@ vim.g.loaded_netrwPlugin = 1
 require("nvim-tree").setup({
   view = {
     adaptive_size = true
+  },
+  actions = {
+    open_file = {
+      quit_on_open = true,
+    },
   }
 })
 
 vim.keymap.set('n', '<Leader>nc', ':NvimTreeClose<CR>', { desc = "Nvimtree close" })
 vim.keymap.set('n', '<Leader>no', ':NvimTreeOpen<CR>', { desc = "Nvimtree open" })
 vim.keymap.set('n', '<Leader>e', ':NvimTreeFocus<CR>', { desc = "Nvimtree toggle focus" })
-
