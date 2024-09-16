795f379c3d226fa130e4c1570391bd653e9b2595
David Håland <david.f.haland@gmail.com>
2024-09-16 08:29:06 +0200

    vimtex

--- lua/core/plugin_config/latex.lua
+++ lua/core/plugin_config/latex.lua
@@ -1,18 +1,19 @@
--- Necessary for VimTeX to load properly
+-- Enable filetype detection and plugins, necessary for VimTeX
 vim.cmd('filetype plugin indent on')
 
 -- Enable syntax highlighting
 vim.cmd('syntax enable')
 
--- Choose one of the following viewer configurations:
--- If you want to use zathura:
-vim.g.vimtex_view_method = 'zathura'
+-- Set the custom viewer for VimTeX to SumatraPDF with the full path
+vim.g.vimtex_view_method = 'SumatraPDF'
+vim.g.vimtex_view_general_viewer = 'C:/Users/david/AppData/Local/SumatraPDF/SumatraPDF.exe'
+vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
 
 
 -- Change the local leader key (optional)
 vim.g.maplocalleader = ","
 
--- Key mappings (if using Neovim 0.7 or later)
+-- Key mappings for VimTeX (using Neovim 0.7+)
 vim.keymap.set('n', '<Leader>lt', ':VimtexTocToggle<CR>', { desc = "Vimtex Table of Contents" })
 vim.keymap.set('n', '<Leader>ll', ':VimtexCompile<CR>', { desc = "Vimtex Compile" })
 vim.keymap.set('n', '<Leader>lv', ':VimtexView<CR>', { desc = "Vimtex View" })
