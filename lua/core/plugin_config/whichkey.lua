6604e70d6da21da30ca7328f48bcc49bc66b11fd
David Håland <david.haland@bouvet.no>
2024-09-18 09:38:21 +0200

    witchkey bugfix

--- lua/core/plugin_config/whichkey.lua
+++ lua/core/plugin_config/whichkey.lua
@@ -4,7 +4,7 @@ wk.add({
   { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
   { "<leader>fb", function() print("hello") end,   desc = "Foobar" },
   { "<leader>fn", desc = "New File" },
-  { "<leader>f1", hidden = true },                     -- hide this keymap
+  { "<leader>f1", hidden = true },                                      -- hide this keymap
   { "<leader>w",  proxy = "<c-w>",                 group = "windows" }, -- proxy to window mappings
   {
     "<leader>b",
@@ -17,8 +17,6 @@ wk.add({
     -- Nested mappings are allowed and can be added in any order
     -- Most attributes can be inherited or overridden on any level
     -- There's no limit to the depth of nesting
-    mode = { "n", "v" },                          -- NORMAL and VISUAL mode
-    { "<leader>q", "<cmd>q<cr>", desc = "Quit" }, -- no need to specify mode since it's inherited
-    { "<leader>w", "<cmd>w<cr>", desc = "Write" },
+    mode = { "n", "v" }
   }
 })
