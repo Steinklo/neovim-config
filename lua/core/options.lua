617e6ef77efd0fe62c242a3fd2b8d21c448dbe27
David Håland <david.f.haland@gmail.com>
2024-09-22 09:19:06 +0200

    formating for latex files

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -23,9 +23,46 @@ vim.opt.updatetime = 50
 
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-    group = highlight_group,
-    pattern = '*',
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
 })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "tex",
+  callback = function()
+    vim.b.copilot_enabled = false
+  end,
+})
+
+
+if vim.fn.has('autocmd') == 1 then
+  vim.api.nvim_create_augroup("format_tex_on_save", { clear = true })
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    group = "format_tex_on_save",
+    pattern = "*.tex",
+    callback = function()
+      local lines = vim.fn.getline(1, '$')
+      local new_lines = {}
+
+      for _, line in ipairs(lines) do
+        while #line > 80 do
+          local linePos = 80
+          for i = 80, 1, -1 do
+            if line:sub(i, i):match("[,%. ]") then
+              linePos = i
+              table.insert(new_lines, line:sub(1, linePos))
+              break
+            end
+          end
+          line = line:sub(linePos + 1)
+        end
+        table.insert(new_lines, line)
+      end
+
+      vim.fn.setline(1, new_lines)
+    end,
+  })
+end
+
