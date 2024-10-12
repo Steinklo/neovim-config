c69355a60999b047539ce155785b4862f5f68e66
David Håland <david.f.haland@gmail.com>
2024-10-12 11:02:07 +0200

    moved some function around

--- lua/core/plugin_config/latex.lua
+++ lua/core/plugin_config/latex.lua
@@ -20,3 +20,41 @@ vim.keymap.set('n', '<Leader>lv', ':VimtexView<CR>', { desc = "Vimtex View" })
 vim.keymap.set('n', '<Leader>ls', ':VimtexStop<CR>', { desc = "Vimtex Stop" })
 vim.keymap.set('n', '<Leader>lc', ':VimtexClean<CR>', { desc = "Vimtex Clean" })
 vim.keymap.set('n', '<Leader>lr', ':VimtexRecompile<CR>', { desc = "Vimtex Recompile" })
+
+function WordCount()
+  local wc = vim.fn.wordcount()
+  print("Words: " .. wc.words)
+end
+
+vim.keymap.set("n", "<leader>wc", WordCount, { desc = "Count words in buffer" })
+vim.keymap.set("n", "<leader>lw", WordCount, { desc = "Count words in buffer" })
+
+
+if vim.fn.has('autocmd') == 1 then
+  vim.api.nvim_create_augroup("format_tex_on_save", { clear = true })
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    group = "format_tex_on_save",
+    pattern = "*.tex",
+    callback = function()
+      local lines = vim.fn.getline(1, '$')
+      local new_lines = {}
+
+      for _, line in ipairs(lines) do
+        while #line > 80 do
+          local linePos = 80
+          for i = 80, 1, -1 do
+            if line:sub(i, i):match("[,%. ]") then
+              linePos = i
+              table.insert(new_lines, line:sub(1, linePos))
+              break
+            end
+          end
+          line = line:sub(linePos + 1)
+        end
+        table.insert(new_lines, line)
+      end
+
+      vim.fn.setline(1, new_lines)
+    end,
+  })
+end
